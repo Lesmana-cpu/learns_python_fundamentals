b5c3fb6b3d5567e08bee9d6a47608057b4b26162
lesmanadarajat <lesmanadarajat42@gmail.com>
2025-06-20 17:26:39 +0700

    upload files

---
+++ about_OOP/private_variable.py
@@ -0,0 +1,40 @@
+import os
+os.system("cls" if os.name == "nt" else "clear")
+
+class Hero:
+    
+    # class variable
+    jumlah = 0
+    __privateJumlah = 0  # private class variable
+    
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+        
+        # variable instance private
+        self.__private = "ini private"
+        
+        # variable instance protected
+        self._protected = "ini protected"
+        
+jenifer = Hero("Jennifer", 125)
+
+print(jenifer.__dict__)
+jenifer.name = "Rose"
+print(jenifer.__dict__)
+print("\n")
+
+# private
+jenifer.__private = "change testing"
+print(jenifer.__dict__)
+print(jenifer.__private)
+print("\n")
+
+# protected
+jenifer._protected = "change proteksi"
+print(jenifer.__dict__)
+print(jenifer._protected)
+print("\n")
+
+print(Hero.__dict__)
+print(Hero.__privateJumlah)

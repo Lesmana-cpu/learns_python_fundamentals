0a18ce8e194d972a532d0dcf6b655d78ed867470
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-08-04 19:11:15 +0700

    upload files

---
+++ about_OOP/class_abstrack.py
@@ -0,0 +1,25 @@
+# class abstract
+# abc = abstract base class
+from abc import ABC, abstractclassmethod
+
+class Button(ABC):
+    
+    @abstractclassmethod
+    def click(self):
+        # print("button click")
+        pass
+
+class PushButton(Button):
+    
+    def click(self):
+        print('push button')
+        
+        
+tombol1 = PushButton()
+
+tombol1.click()
+help(tombol1)
+
+        
+    
+    
